12e2de16360e03b0d6b798b83854b44f236e2049
qqibrow <qqibrow@gmail.com>
2015-08-29 14:01:23 -0700

    add macro to easy create waterfall object

--- tests/WaterfallTest.cpp
+++ tests/WaterfallTest.cpp
@@ -64,7 +64,7 @@ struct Waterfall<A, B, C> {
 template<typename T, typename... Ts>
 struct Waterfall<T, Ts...> {
     using HeadType = typename boost::tuple<Ts...>::head_type;
-    using TailType = typename std::tuple_element<std::tuple_size<std::tuple<Ts...>>::value-1, std::tuple<Ts...>>::type;
+    using TailType = typename std::tuple_element<std::tuple_size<std::tuple<Ts...>>::value - 1, std::tuple<Ts...>>::type;
 
     Waterfall(typename CallbackStruct<T, HeadType>::f func, Waterfall<Ts...> rest): func_(func), rest_(rest) {}
 
@@ -78,31 +78,38 @@ struct Waterfall<T, Ts...> {
     Waterfall<Ts...> rest_;
 };
 
+// 1 Waterfall<A,B>(f1)
+// 2 Waterfall<A,B,C>(f1, Waterfall<B,C>(f2)
+// 3 Waterfall<A,B,C,D>(f1, Waterfall<B,C,D>(f2, Waterfall<C,D>(f3))
+#define WATERFALL_2(T1,T2,f1) Waterfall<T1, T2>(f1)
+#define WATERFALL_3(T1,T2,T3,f1,f2)  Waterfall<T1,T2,T3>(f1, WATERFALL_2(T2,T3,f2))
+#define WATERFALL_4(T1,T2,T3,T4,f1,f2,f3)  Waterfall<T1,T2,T3,T4>(f1,WATERFALL_3(T2,T3,T4,f2,f3))
+#define WATERFALL_5(T1,T2,T3,T4,T5,f1,f2,f3,f4)  Waterfall<T1,T2,T3,T4,T5>(f1,WATERFALL_4(T2,T3,T4,T5,f2,f3,f4))
+
 TEST_F(WaterfallTest, TestWaterfallTwo) {
-    Waterfall<int, string> twoparams([](int a, Callback<string>::f callback) {
-        std::stringstream s;
-        s << a;
-        callback(s.str());
-    });
+    auto twoparams = WATERFALL_2(int, string,
+            [](int a, Callback<string>::f callback) {
+                std::stringstream s;
+                s << a;
+                callback(s.str());
+            }
+    );
     twoparams(121, [](string s) {
         cout << s << endl;
     });
 }
 
 TEST_F(WaterfallTest, TestWaterfallThree) {
-    Waterfall<int, string, double> threeparams(
+    auto threeparams = WATERFALL_3(int, string, double,
             [](int a, Callback<string>::f callback) {
                 std::stringstream s;
                 s << a;
                 callback(s.str());
             },
-            Waterfall<string, double>(
-                    [](string s, Callback<double>::f callback) {
-                        callback(34.56);
-                    }
-            )
+            [](string s, Callback<double>::f callback) {
+                callback(34.56);
+            }
     );
-
     threeparams(1234, [](double s){
         cout << s << endl;
     });
